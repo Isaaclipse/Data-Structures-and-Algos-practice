aab7dc0a92cc4fde9748de73c706e5a3664ed9ea
Isaac A <isaaca365@gmail.com>
2021-02-11 12:13:49 -0500

    Day 6
    added day 6 problem

---
+++ Leetcode-206-Reverse-Linked-List/Main.java
@@ -0,0 +1,18 @@
+// Leetcode #206. Reverse Linked List
+
+// Reverse a singly linked list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+
+// Follow up:
+
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Leetcode #206. Reverse Linked List");
+  }
+}
